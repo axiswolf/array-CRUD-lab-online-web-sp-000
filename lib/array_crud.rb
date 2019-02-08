153839ce5861de20ee8a86d3a3355ecf1fdff74d
Karen V <karenvo@usc.edu>
2019-02-08 14:32:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  index_number.to_i
+  index_number = index_number.to_i
   array['#{index_number}']
 end
 
